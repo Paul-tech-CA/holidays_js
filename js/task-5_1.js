c168115137d28fcbb6cf4c9a9a8f8438b8db7c5f
Pavel Sakun <axelhomkin@gmail.com>
2020-12-27 00:21:29 +0200

    task 14

---
+++ js/task-5_1.js
@@ -0,0 +1,15 @@
+'use strict';
+// ___________За допомогою оператора визначення типу переконайтеся,
+//  що змінних належать типам: string, number, boolean.
+
+const str = 'qwerty';
+const num = 3.14;
+const boolean = false;
+const object = {
+  a: 1,
+  b: 'b',
+  c: true,
+};
+const array = [1, 2, 3, 4, 5];
+
+console.log(typeof str);
